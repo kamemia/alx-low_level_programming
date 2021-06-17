3806b2616b26ca152ba9115e41e08a5bcd8b4f21
kamemia1 <kamemia18@yahoo.com>
2021-06-17 07:11:20 -0700

    4.Hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
-int main()
+/**
+ *main - print the string into the output
+ *
+ *Using the main function
+ *this program prints out "Programming is like building a multilingual puzzle
+ *Return: 0;
+ */
+int main(void)
 {
-  puts("Programming is like building a multilingual puzzle");
+  puts("\"Programming is like building a multilingual puzzle");
   return 0;
 }
