b55765895732a1f902ad97e4801211294be4288b
kamemia1 <kamemia18@yahoo.com>
2021-06-17 13:44:54 -0700

    fix

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,7 +11,7 @@ int i;
 long li;
 long long lli;
 float f;  
-printf("Size of a char: %ld bytes(s)\n", sizeof(c));
+printf("Size of a char: %ld byte(s)\n", sizeof(c));
 printf("Size of an int: %ld byte(s)\n", sizeof(i));
 printf("Size of a long int: %ld byte(s)\n", sizeof(li));
 printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
