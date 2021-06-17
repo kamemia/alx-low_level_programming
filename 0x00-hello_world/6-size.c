fce7931d317a6da3fb68f3b0052a0463152818cc
kamemia1 <kamemia18@yahoo.com>
2021-06-17 13:32:31 -0700

    Sizeofc

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ int main(void)
   long long lli;
   float f;
   
-printf("Size of char: %ld bytes(s)\n", sizeof(c));
-printf("Size of int: %ld byte(s)\n", sizeof(i));
+printf("Size of a char: %ld bytes(s)\n", sizeof(c));
+printf("Size of an int: %ld byte(s)\n", sizeof(i));
 printf("Size of a long int: %ld byte(s)\n", sizeof(li));
 printf("Size of a long long int: %ld  byte(s)\n", sizeof(lli));
-printf("Size of float: %ld byte(s)\n", sizeof(f));
+printf("Size of a float: %ld byte(s)\n", sizeof(f));
 return (0);
 }
