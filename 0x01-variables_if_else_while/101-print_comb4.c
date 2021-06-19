23c47332f8e383f1fd6b30941959a5f0a2a55365
kamemia1 <kamemia18@yahoo.com>
2021-06-18 22:20:46 -0700

    Fix

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,30 +10,30 @@
  */
 int main(void)
 {
-  int i = 48, j = 49, y = 50;
-  while (i < 58)
-    {
-      while (j < 58)
-	{
-	  while (y < 58)
-	    {
-	      putchar(i);
-	      putchar(j);
-	      putchar(y);
+int i = 48, j = 49, y = 50;
+while (i < 58)
+{
+while (j < 58)
+{
+while (y < 58)
+{
+putchar(i);
+putchar(j);
+putchar(y);
 
-	      if (i != 55 || j != 56 || y != 57)
-		{
-		  putchar(',');
-		  putchar(' ');
-		}
-	      y++;
-	    }
-	  j++;
-	  y = j + 1;
-	}
-      i++;
-      j = i;
-    }
-  putchar('\n');
-  return (0);
+if (i != 55 || j != 56 || y != 57)
+{
+putchar(',');
+putchar(' ');
+}
+y++;
+}
+j++;
+y = j + 1;
+}
+i++;
+j = i;
+}
+putchar('\n');
+return (0);
 }
