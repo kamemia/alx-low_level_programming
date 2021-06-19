18f681392b4ddfd0a302fecaa518de4b3b23b1d3
kamemia1 <kamemia18@yahoo.com>
2021-06-18 21:47:19 -0700

    Fix

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,13 +8,13 @@
  */
 int main(void)
 {
-  int letter;
+int letter;
 
-  for (letter = 'a'; letter <= 'z'; letter++)
-    putchar(letter);
-  for (letter = 'A'; letter <= 'Z'; letter++)
-    putchar(letter);
+for (letter = 'a'; letter <= 'z'; letter++)
+putchar(letter);
+for (letter = 'A'; letter <= 'Z'; letter++)
+putchar(letter);
 
-  putchar('\n');
-  return (0);
+putchar('\n');
+return (0);
 }
