30b8a9a178bca695733e3e207b968413c0450ae7
kamemia1 <kamemia18@yahoo.com>
2021-06-18 21:56:22 -0700

    Fix

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -8,10 +8,10 @@
  */
 int main(void)
 {
-  int num;
+int num;
 
-  for (num = 0; num < 10; num++)
-    putchar(num + '0');
-  putchar('\n');
-  return (0);
+for (num = 0; num < 10; num++)
+putchar(num + '0');
+putchar('\n');
+return (0);
 }
