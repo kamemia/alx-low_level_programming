0895b096c06b2ff2fd0e9a6cfbe8cf2e64260d45
kamemia1 <kamemia18@yahoo.com>
2021-06-18 21:44:58 -0700

    Fix

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,10 +8,10 @@
  */
 int main(void)
 {
-  int letter;
+int letter;
 
-  for (letter = 'a'; letter <= 'z'; letter++)
-    putchar(letter);
-  putchar('\n');
-  return (0);
+for (letter = 'a'; letter <= 'z'; letter++)
+putchar(letter);
+putchar('\n');
+return (0);
 }
