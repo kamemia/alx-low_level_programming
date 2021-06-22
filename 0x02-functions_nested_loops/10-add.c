0887b4f334e374188277876aad53a2394b5f9494
kamemia1 <kamemia18@yahoo.com>
2021-06-22 08:57:55 -0700

    Daily task

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,11 +1,13 @@
-include "holberton.h"
+#include "holberton.h"
+
 /**
- * add - adds two numbers together.
- * @i: The first number to be added.
- * @j: The second number to be added.
- * Return: the sum of two numbers.
- */
-int add(int i, int j)
+  * add - Adds two values
+  * @s1: The first number to be computed
+  * @s2: The second number to be computed
+  *
+  * Return: Integer value
+  */
+int add(int s1, int s2)
 {
-return (i + j);
+return (s1 + s2);
 }
